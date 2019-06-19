a728c9df9f9db5a62009f024b15eaf88d20a0816
Vchalem <Vchalem@gmail.com>
2019-06-19 17:10:19 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -8,12 +8,13 @@ sfdx force:package:install --package 04t1U0000066Af4 -w 20
 sfdx force:package:install --package 04t1U000005ZS27 -w 20  
 
 
-sfdx force:source:push -f
-
 #sfdx assign permission sets
 sfdx force:user:permset:assign -n Credit_Line_Increase_Demo
 #assign perm set for APs
 sfdx force:user:permset:assign -n ActionPlans
+
+sfdx force:source:push -f
+
 
 # Include the comprehensive plan to load all data items
 sfdx force:data:tree:import --plan data/Plan1.json
